8fdd46e910ee6c7b194667d0602a6f4b5d49fea5
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-03-20 20:28:15 -0400

    Create 0001.hs

---
+++ bad-hs-code/0001.hs
@@ -0,0 +1,11 @@
+import Control.Concurrent;
+-- The above import is wholly unnecessary.
+import Text.Printf;
+
+main = do
+  -- Real men avoid do notation.
+  -- See http://wiki.haskell.org/Do_notation_considered_harmful.
+  printf "Fuck you.\n";
+  -- putStrLn can be used, thereby removing the Text.Printf dependency.
+  return 1;
+  -- return is not used as return is used above.
