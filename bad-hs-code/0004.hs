58612164c6550fdad470bb1d0e380df3338113d6
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-04-06 20:43:37 -0400

    Create 0004.hs

---
+++ bad-hs-code/0004.hs
@@ -0,0 +1,16 @@
+import System.Environment;
+
+party :: String -> IO ();
+party x = putStrLn $ show (read x :: Integer);
+-- For all G, for all k, show (read k :: G) = k.
+-- As such, function party can be simplified.
+-- In fact, function party is simplified -- see function party'.
+
+party' :: String -> IO ();
+party' = putStrLn;
+-- Creating an alias of putStrLn is weird, but function party is
+-- equivalent to putStrLn.
+
+main :: IO ()
+main = getArgs >>= party . (!! 0);
+-- main is fine.
