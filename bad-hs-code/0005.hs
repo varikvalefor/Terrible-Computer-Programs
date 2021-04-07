dbbdceea9f09c96d82f1f9af55b9082df4426555
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-04-06 20:44:34 -0400

    Create 0005.hs

---
+++ bad-hs-code/0005.hs
@@ -0,0 +1,38 @@
+import Control.Monad;
+import System.Environment;
+
+kenny :: a -> Maybe b;
+kenny g = (return >=> \a -> Nothing) g;
+-- kenny contains a decent number of wasted bytes.
+--
+-- kenny's use of the KLEISLI composition operator is
+-- completely unnecessary.  For all {g, function f of
+-- type (g -> x)}, (return g >=> f) == f g.
+--
+-- kenny' is a simplified version of kenny.
+
+kenny' :: a -> Maybe b;
+kenny' g = Nothing;
+
+main :: IO ();
+main = getArgs >>= mapM_(return . kenny >=>
+  maybe (putStrLn "Aw, shit.") putStrLn);
+-- Like kenny, main contains an unnecessary KLEISLI
+-- composition operator.
+--
+-- Additionally, a space should be placed between "mapM_"
+-- and "(return".
+--
+-- main' is a bettered version of main.
+
+main' :: IO ();
+main' = getArgs >>= mapM_ 
+  (maybe (putStrLn "Aw, shit.") putStrLn . kenny);
+-- Alternatively...
+
+main'' :: IO ();
+main'' = getArgs >>= mapM_ (party . kenny) 
+  where party = maybe (putStrLn "Aw, shit.") putStrLn;
+
+-- Because the KLEISLI composition operators are removed,
+-- the Control.Monad dependency no longer exists.
