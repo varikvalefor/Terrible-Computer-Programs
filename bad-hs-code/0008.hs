ff3eb1a4798b45ad3c080e7fcd611817d16b85a5
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-04-16 21:49:19 -0400

    Create 0008.hs

---
+++ bad-hs-code/0008.hs
@@ -0,0 +1,30 @@
+import Data.List;
+
+isPrime :: Integer -> Bool;
+isPrime k = [a | a <- [2..k - 1], mod k a == 0] == [];
+-- The above list comprehension is fairly long and not terribly
+-- unreadable.
+--
+-- isPrime is re-written as isPrime'.
+
+isPrime' :: Integer -> Bool;
+isPrime' k = not $ any ((== 0) . mod k) [2..k - 1];
+
+thePrimes :: [Integer];
+thePrimes = [a | a <- [2..100000], isPrime a];
+-- The use of list comprehensions is generally discouraged.
+--
+-- thePrimes is re-written as thePrimes'.
+
+thePrimes' :: [Integer];
+thePrimes' = filter isPrime' [2..100000];
+
+--main :: IO ();
+main = putStrLn $ foldr (++) [] $ map (\ (a,b) -> show a ++ ", " ++ show b) thePrim
+  where thePrim = zip thePrimes (tail thePrimes)
+-- main is overall a somewhat confusing trainwreck.
+--
+-- The output of main' equals the output of main.
+
+main' :: IO ();
+main' = mapM_ (putStr . (++ ", ") . show) thePrimes;
