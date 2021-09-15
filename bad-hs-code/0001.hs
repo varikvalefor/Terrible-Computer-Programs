ef96ca1dc9827836692ac99f45411f14eddc1cdb
Varik Valefor <38414004+varikvalefor@users.noreply.github.com>
2021-09-15 04:35:04 -0400

    0001.hs: A qualifying word is added.

--- bad-hs-code/0001.hs
+++ bad-hs-code/0001.hs
@@ -3,7 +3,7 @@ import Control.Concurrent;
 import Text.Printf;
 
 main = do
-  -- Real men avoid do notation.
+  -- Real men avoid unnecessary do notation.
   -- See http://wiki.haskell.org/Do_notation_considered_harmful.
   printf "Fuck you.\n";
   -- putStrLn can be used, thereby removing the Text.Printf dependency.
